e758f2a642df78888946f91812752bc7621c308b
Avesh <avesh.webosmotic@gmail.com>
2023-07-21 18:25:06 +0530

    Create Material UI Theme

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  components: {
+    // myBox: {
+    //   display: "flex",
+    //   justifyContent: "space-between",
+    //   alignItems: "center",
+    // },
+  },
+});
