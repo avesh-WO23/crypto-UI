bb6a47e4db2ad2883d6457d0f838f8f59f1ac113
Harsh-WO <harsh.webosmotic@gmail.com>
2023-07-31 17:52:07 +0530

    Feat: State Selection in filter drawer added, and searchParams added for selected states

--- src/components/results/filters/CollapsibleAccordion.jsx
+++ src/components/results/filters/CollapsibleAccordion.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Box } from '@mui/material';
@@ -7,22 +7,123 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { useSearchParams } from 'react-router-dom';
 
 const LOCATIONS = [
-  { country: 'All' },
   { country: 'USA', states: ['Alabama', 'Georgia', 'Missisipi'] },
   { country: 'Colombia', states: ['Amazonas', 'Antioquia', 'Arauca'] },
+  { country: 'India', states: ['Gujarat', 'Punjab', 'Maharashtra'] },
 ];
 
 export default function CollapsibleAccordion() {
-  const [checked, setChecked] = React.useState([true, false]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [selectedLocation, setSelectedLocation] = React.useState(() => {
+    const getLengthStates = (country) => {
+      return LOCATIONS.find((c) => c.country === country)?.states?.length;
+    };
+    return {
+      isAll: function (country) {
+        return this?.[country]?.states?.length === getLengthStates(country);
+      },
+      isPartial: function (country) {
+        return (
+          this?.[country]?.states?.length > 0 &&
+          this?.[country]?.states?.length !== getLengthStates(country)
+        );
+      },
+    };
+  });
 
-  const handleChange1 = (event) => {
-    setChecked([event.target.checked, event.target.checked]);
+  const isAllSelected = useMemo(() => {
+    let allSelected = true;
+    const allCountriesName = LOCATIONS.map((c) => c.country);
+    for (let name of allCountriesName) {
+      if (!selectedLocation?.isAll?.(name)) {
+        allSelected = false;
+        break;
+      }
+    }
+    return allSelected;
+  }, [selectedLocation]);
+
+  const handleChange = (country, state) => (event) => {
+    if (state) {
+      setSelectedLocation((prev) => ({
+        ...prev,
+        [country]: {
+          states: prev?.[country]?.states.includes(state)
+            ? prev?.[country]?.states.filter((e) => e !== state)
+            : prev?.[country]?.states?.length > 0
+            ? [...(prev?.[country]?.states || []), state]
+            : [state],
+        },
+      }));
+    } else {
+      const isAlreadyInList =
+        [country] in selectedLocation && selectedLocation?.isAll?.(country);
+      setSelectedLocation((prev) => ({
+        ...prev,
+        [country]: {
+          states: isAlreadyInList
+            ? []
+            : LOCATIONS.find((c) => c.country === country)?.states,
+        },
+      }));
+    }
   };
+
+  useEffect(() => {
+    const states = searchParams.get('states');
+    setSelectedLocation((prev) => {
+      return {
+        ...prev,
+        ...JSON.parse(states),
+      };
+    });
+  }, [searchParams]);
+
+  useEffect(() => {
+    const contries = { ...selectedLocation };
+    delete contries.isAll;
+    delete contries.isPartial;
+
+    setSearchParams({ states: JSON.stringify(contries) });
+  }, [selectedLocation, setSearchParams]);
+
+  const handleAllCountry = (country) => (event) => {
+    if (country === 'All') {
+      setSelectedLocation((prev) => {
+        let countries = {};
+        if (!isAllSelected) {
+          LOCATIONS.map((c) => (countries[c.country] = { states: c.states }));
+        }
+        return {
+          isAll: prev.isAll,
+          isPartial: prev.isPartial,
+          ...countries,
+        };
+      });
+    }
+  };
+
+  const getStateChecked = useCallback(
+    (country, state) => {
+      return !!selectedLocation?.[country]?.states?.includes(state);
+    },
+    [selectedLocation]
+  );
 
   return (
     <Box pt={1}>
+      <FormControlLabel
+        label={'All'}
+        control={
+          <Checkbox
+            checked={isAllSelected}
+            onChange={handleAllCountry('All')}
+          />
+        }
+      />
       {LOCATIONS.map((location, ind) => {
         return (
           <Accordion
@@ -35,10 +136,9 @@ export default function CollapsibleAccordion() {
           >
             <AccordionSummary
               expandIcon={location.states ? <ExpandMoreIcon /> : <></>}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
               sx={{
                 'minHeight': 0,
+                'px': 0,
                 '& .MuiAccordionSummary-content': { margin: 0 },
               }}
             >
@@ -46,19 +146,17 @@ export default function CollapsibleAccordion() {
                 label={location.country}
                 control={
                   <Checkbox
-                    checked={checked[0] && checked[1]}
-                    indeterminate={checked[0] !== checked[1]}
-                    onChange={handleChange1}
+                    checked={selectedLocation?.isAll?.(location.country)}
+                    indeterminate={selectedLocation?.isPartial?.(
+                      location.country
+                    )}
+                    onChange={handleChange(location.country)}
+                    onClick={(e) => e.stopPropagation()}
                   />
                 }
               />
             </AccordionSummary>
-            <AccordionDetails
-              sx={{
-                'padding': 0,
-                '& .MuiBox-root': { marginLeft: 4 },
-              }}
-            >
+            <AccordionDetails sx={{ p: 0 }}>
               {location.states ? (
                 location.states.map((state, i) => (
                   <Box
@@ -71,8 +169,8 @@ export default function CollapsibleAccordion() {
                       label={state}
                       control={
                         <Checkbox
-                          checked={checked[0]}
-                          onChange={handleChange1}
+                          checked={getStateChecked(location.country, state)}
+                          onChange={handleChange(location.country, state)}
                         />
                       }
                     />
